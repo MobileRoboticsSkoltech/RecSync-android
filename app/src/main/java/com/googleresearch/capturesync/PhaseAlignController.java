e3c0625fc415dcf43abac7d6cf949c8fa17dc7b7
azaat <a.r.akhmetyanov@gmail.com>
2021-04-16 13:47:20 +0300

    Change period and max iterations

--- app/src/main/java/com/googleresearch/capturesync/PhaseAlignController.java
+++ app/src/main/java/com/googleresearch/capturesync/PhaseAlignController.java
@@ -36,7 +36,7 @@ public class PhaseAlignController {
   // Maximum number of phase alignment iteration steps in the alignment process.
   // TODO(samansari): Make this a parameter that you pass in to this class. Then make the class that
   // constructs this pass the constant in.
-  private static final int MAX_ITERATIONS = 5;
+  private static final int MAX_ITERATIONS = 50;
   // Delay after an alignment step to wait for phase to settle before starting the next iteration.
   private static final int PHASE_SETTLE_DELAY_MS = 400;
   private final MainActivity context;
